89f9c15824f13077e2f4f08d652b62b5a6f9891f
koolleeo <86853558+koolleeo@users.noreply.github.com>
2023-01-31 16:45:41 +0000

    javascript updated. Added function to catch error, updated wording of questions, and formatted license object to make it more readable

--- utils/licenses.js
+++ utils/licenses.js
@@ -1,40 +1,108 @@
 // object of license badges
 const licenses = {
     
-"Academic Free License v3.0" : { imageURL: "https://img.shields.io/badge/License-afl--3.0-brightgreen.svg"},
-"Apache license 2.0" : { imageURL: "https://img.shields.io/badge/License-apache--2.0-brightgreen.svg"},
-"Artistic license 2.0" : { imageURL: "https://img.shields.io/badge/License-artistic--2.0-brightgreen.svg"},
-"Boost Software License 1.0" : { imageURL: "https://img.shields.io/badge/License-bsl--1.0-brightgreen.svg"},
-"BSD 2-clause 'Simplified' license" : { imageURL: "https://img.shields.io/badge/License-bsd--2--clause-brightgreen.svg"},
-"BSD 3-clause 'New' or 'Revised' license" : { imageURL: "https://img.shields.io/badge/License-bsd--3--clause-brightgreen.svg"},
-"BSD 3-clause Clear license" : { imageURL: "https://img.shields.io/badge/License-bsd--3--clause--clear-brightgreen.svg"},
-"Creative Commons license family" : { imageURL: "https://img.shields.io/badge/License-cc-brightgreen.svg"},
-"Creative Commons Zero v1.0 Universal" : { imageURL: "https://img.shields.io/badge/License-cc0--1.0-brightgreen.svg"},
-"Creative Commons Attribution 4.0" : { imageURL: "https://img.shields.io/badge/License-cc--by--4.0-brightgreen.svg"},
-"Creative Commons Attribution Share Alike 4.0" : { imageURL: "https://img.shields.io/badge/License-cc--by--sa--4.0-brightgreen.svg"},
-"Do What The F*ck You Want To Public License" : { imageURL: "https://img.shields.io/badge/License-wtfpl-brightgreen.svg"},
-"Educational Community License v2.0" : { imageURL: "https://img.shields.io/badge/License-ecl--2.0-brightgreen.svg"},
-"Eclipse Public License 1.0" : { imageURL: "https://img.shields.io/badge/License-epl--1.0-brightgreen.svg"},
-"Eclipse Public License 2.0" : { imageURL: "https://img.shields.io/badge/License-epl--2.0-brightgreen.svg"},
-"European Union Public License 1.1" : { imageURL: "https://img.shields.io/badge/License-eupl--1.1-brightgreen.svg"},
-"GNU Affero General Public License v3.0" : { imageURL: "https://img.shields.io/badge/License-agpl--3.0-brightgreen.svg"},
-"GNU General Public License family" : { imageURL: "https://img.shields.io/badge/License-gpl-brightgreen.svg"},
-"GNU General Public License v2.0" : { imageURL: "https://img.shields.io/badge/License-gpl--2.0-brightgreen.svg"},
-"GNU General Public License v3.0" : { imageURL: "https://img.shields.io/badge/License-gpl--3.0-brightgreen.svg"},
-"GNU Lesser General Public License family" : { imageURL: "https://img.shields.io/badge/License-lgpl-brightgreen.svg"},
-"GNU Lesser General Public License v2.1" : { imageURL: "https://img.shields.io/badge/License-lgpl--2.1-brightgreen.svg"},
-"GNU Lesser General Public License v3.0" : { imageURL: "https://img.shields.io/badge/License-lgpl--3.0-brightgreen.svg"},
-"ISC" : { imageURL: "https://img.shields.io/badge/License-isc-brightgreen.svg"},
-"LaTeX Project Public License v1.3c" : { imageURL: "https://img.shields.io/badge/License-lppl--1.3c-brightgreen.svg"},
-"Microsoft Public License" : { imageURL: "https://img.shields.io/badge/License-ms--pl-brightgreen.svg"},
-"MIT" : { imageURL: "https://img.shields.io/badge/License-mit-brightgreen.svg"},
-"Mozilla Public License 2.0" : { imageURL: "https://img.shields.io/badge/License-mpl--2.0-brightgreen.svg"},
-"Open Software License 3.0" : { imageURL: "https://img.shields.io/badge/License-osl--3.0-brightgreen.svg"},
-"PostgreSQL License" : { imageURL: "https://img.shields.io/badge/License-postgresql-brightgreen.svg"},
-"SIL Open Font License 1.1" : { imageURL: "https://img.shields.io/badge/License-ofl--1.1-brightgreen.svg"},
-"University of Illinois/NCSA Open Source License" : { imageURL: "https://img.shields.io/badge/License-ncsa-brightgreen.svg"},
-"The Unlicense" : { imageURL: "https://img.shields.io/badge/License-unlicense-brightgreen.svg"},
-"zLib License" : { imageURL: "https://img.shields.io/badge/License-zlib-brightgreen.svg"},
+"Academic Free License v3.0" : { 
+    imageURL: "https://img.shields.io/badge/License-afl--3.0-brightgreen.svg"
+},
+"Apache license 2.0" : { 
+    imageURL: "https://img.shields.io/badge/License-apache--2.0-brightgreen.svg"
+},
+"Artistic license 2.0" : { 
+    imageURL: "https://img.shields.io/badge/License-artistic--2.0-brightgreen.svg"
+},
+"Boost Software License 1.0" : { 
+    imageURL: "https://img.shields.io/badge/License-bsl--1.0-brightgreen.svg"
+},
+"BSD 2-clause 'Simplified' license" : { 
+    imageURL: "https://img.shields.io/badge/License-bsd--2--clause-brightgreen.svg"
+},
+"BSD 3-clause 'New' or 'Revised' license" : { 
+    imageURL: "https://img.shields.io/badge/License-bsd--3--clause-brightgreen.svg"
+},
+"BSD 3-clause Clear license" : { 
+    imageURL: "https://img.shields.io/badge/License-bsd--3--clause--clear-brightgreen.svg"
+},
+"Creative Commons license family" : { 
+    imageURL: "https://img.shields.io/badge/License-cc-brightgreen.svg"
+},
+"Creative Commons Zero v1.0 Universal" : { 
+    imageURL: "https://img.shields.io/badge/License-cc0--1.0-brightgreen.svg"
+},
+"Creative Commons Attribution 4.0" : { 
+    imageURL: "https://img.shields.io/badge/License-cc--by--4.0-brightgreen.svg"
+},
+"Creative Commons Attribution Share Alike 4.0" : { 
+    imageURL: "https://img.shields.io/badge/License-cc--by--sa--4.0-brightgreen.svg"
+},
+"Do What The F*ck You Want To Public License" : { 
+    imageURL: "https://img.shields.io/badge/License-wtfpl-brightgreen.svg"
+},
+"Educational Community License v2.0" : { 
+    imageURL: "https://img.shields.io/badge/License-ecl--2.0-brightgreen.svg"
+},
+"Eclipse Public License 1.0" : { 
+    imageURL: "https://img.shields.io/badge/License-epl--1.0-brightgreen.svg"
+},
+"Eclipse Public License 2.0" : { 
+    imageURL: "https://img.shields.io/badge/License-epl--2.0-brightgreen.svg"
+},
+"European Union Public License 1.1" : { 
+    imageURL: "https://img.shields.io/badge/License-eupl--1.1-brightgreen.svg"
+},
+"GNU Affero General Public License v3.0" : { 
+    imageURL: "https://img.shields.io/badge/License-agpl--3.0-brightgreen.svg"
+},
+"GNU General Public License family" : { 
+    imageURL: "https://img.shields.io/badge/License-gpl-brightgreen.svg"
+},
+"GNU General Public License v2.0" : { 
+    imageURL: "https://img.shields.io/badge/License-gpl--2.0-brightgreen.svg"
+},
+"GNU General Public License v3.0" : { 
+    imageURL: "https://img.shields.io/badge/License-gpl--3.0-brightgreen.svg"
+},
+"GNU Lesser General Public License family" : { 
+    imageURL: "https://img.shields.io/badge/License-lgpl-brightgreen.svg"
+},
+"GNU Lesser General Public License v2.1" : { 
+    imageURL: "https://img.shields.io/badge/License-lgpl--2.1-brightgreen.svg"
+},
+"GNU Lesser General Public License v3.0" : { 
+    imageURL: "https://img.shields.io/badge/License-lgpl--3.0-brightgreen.svg"
+},
+"ISC" : { 
+    imageURL: "https://img.shields.io/badge/License-isc-brightgreen.svg"
+},
+"LaTeX Project Public License v1.3c" : { 
+    imageURL: "https://img.shields.io/badge/License-lppl--1.3c-brightgreen.svg"
+},
+"Microsoft Public License" : { 
+    imageURL: "https://img.shields.io/badge/License-ms--pl-brightgreen.svg"
+},
+"MIT" : { 
+    imageURL: "https://img.shields.io/badge/License-mit-brightgreen.svg"
+},
+"Mozilla Public License 2.0" : { 
+    imageURL: "https://img.shields.io/badge/License-mpl--2.0-brightgreen.svg"
+},
+"Open Software License 3.0" : { 
+    imageURL: "https://img.shields.io/badge/License-osl--3.0-brightgreen.svg"
+},
+"PostgreSQL License" : { 
+    imageURL: "https://img.shields.io/badge/License-postgresql-brightgreen.svg"
+},
+"SIL Open Font License 1.1" : { 
+    imageURL: "https://img.shields.io/badge/License-ofl--1.1-brightgreen.svg"
+},
+"University of Illinois/NCSA Open Source License" : { 
+    imageURL: "https://img.shields.io/badge/License-ncsa-brightgreen.svg"
+},
+"The Unlicense" : { 
+    imageURL: "https://img.shields.io/badge/License-unlicense-brightgreen.svg"
+},
+"zLib License" : { 
+    imageURL: "https://img.shields.io/badge/License-zlib-brightgreen.svg"
+},
 
 }
 
